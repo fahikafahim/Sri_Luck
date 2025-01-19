3970f300e9d06b0a2e3b5ff95fec2fd9c21a37b5
fahikafahim <153791032+fahikafahim@users.noreply.github.com>
2025-01-19 14:14:32 +0530

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="path-to-your-tailwind-output.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include '..\layout' ?>
+</body>
+</html>
+
+
+
+
+
+
